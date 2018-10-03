4d83701b8502539388efb6bf34bfab3f1e9a0a2e
Markitanov <dmarkitanov@gmail.com>
2018-10-03 08:46:00 +0600

    final

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
+# Маркитанов Денис КБТУ 2018 Машинное обучение
 import re
 import numpy
 import math
 from matplotlib import pyplot as plt
 
-#Function F(x) given in the task
+# наша функция
 def f(x):
     return math.sin(x / 5) * math.exp(x / 10) + 5 * math.exp(-x / 2)
-#We create a NxN coefficient matrix need in order to solve the system of linear equations
+# создаем матрицу
 def createMatrix(x):
     n = len(x)
     matrix = numpy.zeros(shape=(n, n))
@@ -14,36 +15,32 @@ def createMatrix(x):
         for j in range(n):
             matrix[i][j] = x[i] ** j
     return matrix
-#Create the array of points(f(x[i]), where x[i] ranges from 1 to 15, with a step of 1) on which we will build out function
-def createFunc(rank, scope):
+# раскручиваем решение полиномов n порядка (где n это размерность решений линейной системы уравнений)
+def getSolutions(rank, scope):
     y = numpy.zeros(shape=(len(scope)))
     for i in range(len(scope)):
         for j in range(len(rank)):
-                y[i] += rank[j] * (scope[i] ** j)
+                y[i] += rank[j] * (scope[i] ** j) 
     return y
 
-scope = numpy.arange(1, 15, .1)
-x2 = [1, 15]
-m2 = createMatrix(x2)    
-v2 = list(map(f, x2))
-a2 = numpy.linalg.solve(m2, v2)
+def linearEquationSolver(x): # функция для решения систем линейных уравнений
+    m = createMatrix(x)
+    v = list(map(f, x))
+    return numpy.linalg.solve(m, v) # решение системы линейных уравнений
 
-x3 = [1, 8, 15]
-m3 = createMatrix(x3)    
-v3 = list(map(f, x3))
-a3 = numpy.linalg.solve(m3, v3)
+scope = numpy.arange(1, 15, .1) # делаю полотно для построения графиков
+x = [[1, 15], [1, 8, 15], [1, 4, 10, 15]] # данные икса из задачи
+buffer = [] # будет содержать решения линейных систем
+y = []
 
-x4 = [1, 4, 10, 15]
-m4 = createMatrix(x4)    
-v4 = list(map(f, x4))
-a4 = numpy.linalg.solve(m4, v4)
+for i in range(3):
+    buffer.append(linearEquationSolver(x[i])) # для каждого полинома, который представлен в задаче решаю систему линейных уравнений
                     
-y = list(map(f, scope))
-y2 = createFunc(a2, scope)
-y3 = createFunc(a3, scope)
-y4 = createFunc(a4, scope)
-plt.plot(scope, y)
-plt.plot(scope, y2)
-plt.plot(scope, y3)
-plt.plot(scope, y4)
+yMain = list(map(f, scope))
+for i in range(len(buffer)):
+    y.append(getSolutions(buffer[i], scope)) # нахожу решения полинома n порядка при помощи решенных систем линейного уравнения (A * x = B)
+
+for i in range(len(y)): # рисую
+    plt.plot(scope, y[i])
+plt.plot(scope, yMain)
 plt.show()
